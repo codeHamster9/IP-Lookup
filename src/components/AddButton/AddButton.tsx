b28848a691d9efcf9083943e9f5a84223e187eea
idan <idansagi@gmail.com>
2026-02-11 18:21:02 +0200

    perf: memoize AddButton component
    to prevent unnecessary re-renders

--- src/components/AddButton/AddButton.tsx
+++ src/components/AddButton/AddButton.tsx
@@ -1,13 +1,14 @@
+import { memo } from 'react';
 import styles from './AddButton.module.css';
 
 interface AddButtonProps {
   onClick: () => void;
 }
 
-export function AddButton({ onClick }: AddButtonProps) {
+export const AddButton = memo(function AddButton({ onClick }: AddButtonProps) {
   return (
     <button className={styles.button} onClick={onClick} type="button">
       + Add
     </button>
   );
-}
+});
