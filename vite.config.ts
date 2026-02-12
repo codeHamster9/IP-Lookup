120ce76082eb0fdd258e0fabefe74e3c7220ae21
idan <idansagi@gmail.com>
2026-02-12 14:31:05 +0200

    task: add deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/IP-Lookup/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
